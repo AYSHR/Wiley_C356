55bde197c57280a78ca38be29827f6f714553e68
AYSHR <missamishra09@gmail.com>
2023-05-13 13:14:50 +0530

    sql file

---
+++ From_21_Apr/SQL/sql_practice.sql
@@ -0,0 +1,53 @@
+-- show databases;
+
+-- use wiley;
+
+
+-- create database wiley;
+-- create database sample;
+-- drop database sample;
+
+
+-- command to create  table
+-- create table student(
+-- id int ,
+-- name varchar(30),
+-- grade varchar(5)
+-- );
+
+-- create table wiley_table(
+ -- assignment int,
+ -- course varchar(30)
+-- );
+
+
+
+-- command to view  table and insert data
+-- select * from student;
+-- insert into student values(1,"harry",'a');
+-- insert into student values(2,"any",'a+');
+
+
+
+-- command to view available table
+-- show tables;
+
+
+
+-- command to delete table
+-- drop table student;
+
+
+
+-- to create table
+create table students(
+id int ,
+name varchar(30),
+grade char(2),
+dateofjoin date,
+percentage double
+);
+
+
+
+
